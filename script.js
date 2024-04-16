449952cdaa6a3d440ab4310090199602b4ca8317
mayurthembe07 <112803481+mayurthembe07@users.noreply.github.com>
2024-04-16 23:26:20 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+const inputBox = document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
+function addTask() {
+    if (inputBox.value === '') {
+        alert("Do It ASAP😒");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listcontainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span)
+    }
+    inputBox.value = " ";
+    saveData();
+}
+
+listcontainer.addEventListener("click", function (e){
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listcontainer.innerHTML);
+}
+
+function showList(){
+    listcontainer.innerHTML = localStorage.getItem("data");
+}
+showList();
